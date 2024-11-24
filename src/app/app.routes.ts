9d95e79d3dc8a4919c97853f7fb835bd3da69dad
Pablo Alonso <pabloealonsor@gmail.com>
2024-11-24 14:01:30 -0600

    add home, login, and register components with styles and templates; remove old component files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,20 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent }
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes),
+        NavbarComponent
+    ],
+    exports: [RouterModule],
+})
+export class AppRoutingModule {}
