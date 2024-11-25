71aafdfaf7f64ab8157c2867ff21e3ce30efe4c4
Pablo Alonso <pabloealonsor@gmail.com>
2024-11-25 08:46:16 -0600

    feat: update app configuration to include HttpClientModule; enhance navbar and home components for improved routing and image handling

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,29 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
-import { HttpClient } from '@angular/common/http'; 
 import { VideojuegoServiceService } from '../../services/videojuego-service.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
   standalone: true
 })
-export class HomeComponent {
-  datos: any;
+export class HomeComponent implements OnInit{
+  imageUrl: string = '';
 
   constructor(private videojuegoService: VideojuegoServiceService) {}
 
   ngOnInit() {
-
+    this.getDatos();
   }
 
-  getImagen(): void {
+  getDatos() {
     this.videojuegoService.getJuegoRandom().subscribe(
       (respuesta) => {
-        this.datos = respuesta;
-        console.log('Datos obtenidos:', this.datos);
+        if (respuesta && respuesta.imagenes && respuesta.imagenes.length > 0) {
+          this.imageUrl = respuesta.imagenes[0]; // Ajusta esto según la estructura de tu respuesta
+        }
+        console.log('Datos obtenidos:', this.imageUrl);
       },
       (error) => {
         console.error('Error al obtener datos:', error);
