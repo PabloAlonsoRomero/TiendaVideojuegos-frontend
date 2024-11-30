896096f04d06c86ea2daa40ac2da8b3a6e0e70ce
Pablo Alonso <pabloealonsor@gmail.com>
2024-11-30 11:36:52 -0600

    feat: implement user registration logic in register component; integrate usuario service for user creation

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { Usuario } from '../../interfaces/usuario';
+import { UsuarioService } from '../../services/usuarioService/usuario.service';
 
 @Component({
   selector: 'app-register',
@@ -9,5 +11,24 @@ import { RouterLink } from '@angular/router';
   standalone: true
 })
 export class RegisterComponent {
+  constructor(private usuarioService: UsuarioService) {}
 
+  registrarUsuario(form: any) {
+    const nuevoUsuario: Usuario = {
+      nombre: form.value.user,
+      email: form.value.email,
+      contrasena: form.value.password,
+      telefono: form.value.phone,
+      nombre_usuario: form.value.username
+    }
+
+    this.usuarioService.postUsuario(nuevoUsuario).subscribe(
+      response => {
+        console.log('Usuario registrado:', response);
+      },
+      error => {
+        console.error('Error al registrar usuario:', error)
+      }
+    )
+  }
 }
