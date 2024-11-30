3a028f883c22e70d8cd59708d3b47cc83c4420ef
Pablo Alonso <pabloealonsor@gmail.com>
2024-11-30 11:30:24 -0600

    feat: add usuario service and interface; implement user registration functionality

---
+++ src/app/interfaces/usuario.ts
@@ -0,0 +1,7 @@
+export interface Usuario {
+    nombre: string;
+    email: string;
+    contrasena: string;
+    telefono: string;
+    nombre_usuario: string;
+}
--- src/app/services/usuarioService/usuario.service.ts
+++ src/app/services/usuarioService/usuario.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Usuario } from '../../interfaces/usuario';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuarioService {
+  private baseUrl = "https://tiendavideojuegos-api.onrender.com"
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  postUsuario(usuario: Usuario): Observable<any> {
+    return this.http.post(`${this.baseUrl}/usuario/registro`, {
+        nombre: usuario.nombre,
+        email: usuario.email,
+        contrasena: usuario.contrasena,
+        telefono: usuario.telefono,
+        nombre_usuario: usuario.nombre_usuario
+    });
+  }
 }
