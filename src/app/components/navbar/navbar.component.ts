421fa47c069183eab5d2b6f6922f0b823024a6ae
Pablo Alonso <pabloealonsor@gmail.com>
2024-12-01 13:27:54 -0600

    feat: enhance navbar component with improved dropdown styling and admin check integration

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
+import { UsuarioService } from '../../services/usuarioService/usuario.service';
 
 @Component({
   selector: 'app-navbar',
@@ -16,16 +17,26 @@ export class NavbarComponent {
   admin: boolean = false;
 
   constructor(
-    private router: Router
+    private router: Router,
+    private usuarioService: UsuarioService
   ) {}
 
   ngOnInit() {
     this.isLoggedIn = localStorage.getItem('isLoggedIn') === 'true';
     this.userId = localStorage.getItem('userId');
-    this.admin = localStorage.getItem('admin') === 'true';
 
-    if (this.isLoggedIn) {
+    if (this.isLoggedIn && this.userId) {
       this.nombre_usuario = localStorage.getItem('nombre_usuario');
+      this.usuarioService.getAdmin(this.userId).subscribe(
+        response => {
+          if (response.admin) {
+            this.admin = true;
+          }
+        },
+        error => {
+          console.log("Error al obtener admin");
+        }
+      )
     }
   }
 
@@ -36,6 +47,7 @@ export class NavbarComponent {
     this.isLoggedIn = false;
     this.userId = null;
     this.nombre_usuario = null;
+    this.admin = false;
     this.router.navigate(['/'])
   }
 }
