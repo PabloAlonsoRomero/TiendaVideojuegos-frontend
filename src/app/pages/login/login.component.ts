5eeda7b3f8b2925991c1246dc4b999206b6b89a9
Pablo Alonso <pabloealonsor@gmail.com>
2024-11-30 22:39:34 -0600

    feat: implement reactive form for login and handle user authentication

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,46 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { UsuarioService } from '../../services/usuarioService/usuario.service';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
   standalone: true
 })
 export class LoginComponent {
+  loginForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private usuarioService: UsuarioService,
+    private router: Router
+  ){
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    })
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+
+      this.usuarioService.loginUsuario(email, password).subscribe(
+        response => {
+          console.log("Usuario logueado exitosamente");
+          
+          localStorage.setItem('userId', response.userID);
+          localStorage.setItem('isLoggedIn', 'true');
+
+          this.router.navigate(['/']);
+        },
+        error => {
+          console.log("Error al loguear usuario");
+        }
+      )
+    }
+  }
 }
