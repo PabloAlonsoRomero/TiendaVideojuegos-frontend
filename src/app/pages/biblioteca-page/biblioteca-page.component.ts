86b2585f4f935a4f31e057f466b6641fcc94623b
Pablo Alonso <pabloealonsor@gmail.com>
2024-12-01 01:01:01 -0600

    feat: add biblioteca page component with initial template, styles, and tests

---
+++ src/app/pages/biblioteca-page/biblioteca-page.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-biblioteca-page',
+  imports: [],
+  templateUrl: './biblioteca-page.component.html',
+  styleUrl: './biblioteca-page.component.css'
+})
+export class BibliotecaPageComponent {
+
+}
