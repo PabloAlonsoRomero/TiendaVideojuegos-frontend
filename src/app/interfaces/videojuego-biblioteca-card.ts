0cf3c4bfe6eb46d1b8189f0ac34835f85f8ecfdc
Pablo Alonso <pabloealonsor@gmail.com>
2024-12-01 16:05:35 -0600

    feat: add VideojuegoBibliotecaCard interface for game details representation

---
+++ src/app/interfaces/videojuego-biblioteca-card.ts
@@ -0,0 +1,8 @@
+export interface VideojuegoBibliotecaCard {
+    titulo: string;
+    descripcion: string;
+    genero: string[];
+    desarrollador: string;
+    distribuidor: string;
+    imagen: string;
+}
